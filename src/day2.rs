07f2bdbc917fd9a25580b3fb4004a9152f355a49
Johan Grahn <johan.grahn@me.com>
2024-01-13 22:32:50 +0100

    Add solution for day2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -30,6 +30,33 @@ fn part1(input: &str) -> u32 {
         })
         .sum::<_>()
 }
+
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut map: HashMap<&str, u32> = HashMap::new();
+            let game = line.split(':').last().unwrap();
+            game.split([';', ','])
+                .map(|rev| rev.trim())
+                .for_each(|rev| {
+                    let s = rev.split_whitespace().collect::<Vec<_>>();
+                    let mut number: u32 = s[0].parse().unwrap();
+                    let t = s[1];
+
+                    map.entry(t)
+                        .and_modify(|val| {
+                            if val < &mut number {
+                                *val = number
+                            }
+                        })
+                        .or_insert(number);
+                });
+
+            map.values().product::<u32>()
+        })
+        .sum::<_>()
+}
 #[cfg(test)]
 mod part1 {
     use std::fs::read_to_string;
@@ -48,5 +75,25 @@ mod part1 {
         let input = read_to_string("input/day2.txt").unwrap();
         let result = part1(&input);
         assert_eq!(result, 2207)
+    }
+}
+#[cfg(test)]
+mod part2 {
+    use std::fs::read_to_string;
+
+    use crate::day2::part2;
+
+    #[test]
+    fn test_example() {
+        let input = read_to_string("input/day2_example.txt").unwrap();
+        let result = part2(&input);
+        assert_eq!(result, 2286)
+    }
+
+    #[test]
+    fn test_real_input() {
+        let input = read_to_string("input/day2.txt").unwrap();
+        let result = part2(&input);
+        assert_eq!(result, 62241)
     }
 }
