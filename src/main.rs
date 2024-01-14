e31b2fcef58a5b4fc662e9b777dcfcd22a26402d
Johan Grahn <johan.grahn@me.com>
2024-01-14 11:29:11 +0100

    Some minor cleanups

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::BTreeMap;
 use std::fs::read_to_string;
+
 mod day1;
 mod day2;
+mod day3;
 
 use crate::day1::day1;
 use crate::day2::day2;
+use crate::day3::day3;
 
 type DayFunction = fn(&str) -> (u32, u32);
+
 fn main() {
     let mut map: BTreeMap<usize, DayFunction> = BTreeMap::new();
 
     map.insert(1, day1);
     map.insert(2, day2);
+    map.insert(3, day3);
 
     map.keys().for_each(|key| {
         let function = map.get(key).unwrap();
